97f0f49062e00e9efd59f0a0a9ccc4a6fa0d754c
hugop <h.perez10@usp.ceu.es>
2025-06-13 02:11:27 +0200

    updated MCU code to latest

--- codeMCU/robotControl_Server/robotControl.ino
+++ codeMCU/robotControl_Server/robotControl.ino
@@ -7,6 +7,8 @@
 #define SERVOID_OFFSET 5
 #define SERVOPOS_OFFSET 7
 
+#define ENABLE_TRACES
+
 #include <Arduino.h>
 
 #include <WiFi.h>
@@ -17,8 +19,11 @@
 WiFiClient client;
 String query = "";
 uint8_t i = 0;
-uint8_t cServoID = 0;
+uint8_t cServoID = 0, cServoPos=0;
 uint32_t flag = 0;
+uint16_t pwm=0;
+
+char loginQuery[82];
 
 Adafruit_PWMServoDriver pwm0 = Adafruit_PWMServoDriver(0x40);
 Adafruit_PWMServoDriver pwm1 = Adafruit_PWMServoDriver(0x41);
@@ -35,54 +40,78 @@ void handleQuery(){
   }
   switch(query.charAt(1)){
     case 'm': /* Move servos */
-      // Serial.println("Move Servos");
+      #ifdef ENABLE_TRACES
+        Serial.println("At servo movement execution");
+      #endif
+      
       if(query.length()<(FORMAT_LEN+SERVOPAIR_LEN*query[SERVOCOUNT_OFFSET])){client.print("NACK1");break;} /* Would be Out of bounds */
       if(query[SERVOCOUNT_OFFSET]>SERVOCOUNT){client.print("NACK2");break;} /* Servocounnt missmatch */
       for(i=0;i<query[SERVOCOUNT_OFFSET];i++){
-        cServoID=getServoID();
+
+        cServoID=getServoID(); cServoPos=getServoPos();
+        pwm = minServoPos[cServoID] + ((float)cServoPos / 179.0f) * (maxServoPos[cServoID] - minServoPos[cServoID]);
 
         if(cServoID<6){/* LeftHand Servo Controls (PCA9685 board 0) - Servos 0-5 (6) */
-          Serial.println("pwm0: ServoID->"+String(cServoID)+", Position->"+String((uint8_t)(getServoPos())));
-          // pwm0.setPWM(cServoID,0, getServoPos() * ((maxServoPos[cServoID]-minServoPos[cServoID])/180) ); 
-				}else if(cServoID<12){ /* RightHand Servo Controls (PCA9685 board 2) - Servos 6-11 (6) */
-          Serial.println("pwm2: ServoID->"+String(cServoID)+", Position->"+String((uint8_t)(getServoPos())));
-          // pwm2.setPWM((cServoID-6),0, getServoPos() * ((maxServoPos[cServoID]-minServoPos[cServoID])/180) );   
-				}else{ /* Body/Head Servo Controls (PCA9685 board 1) - Servos 12-26 (15) */
-          Serial.println("pwm1: ServoID->"+String(cServoID)+", Position->"+String((uint8_t)(getServoPos())));
-          // pwm1.setPWM((cServoID-12),0, getServoPos() * ((maxServoPos[cServoID]-minServoPos[cServoID])/180) );   
-				}
+          #ifdef ENABLE_TRACES
+            Serial.printf("pwm0: ServoID -> %u, Position -> %u, PWM signal -> %u, min-maxPWM -> (%u,%u)\n", cServoID, cServoPos, pwm, minServoPos[cServoID], maxServoPos[cServoID]);
+          #endif
 
-        delayMicroseconds(10);
+          pwm0.setPWM(cServoID,0,pwm); 
+          delayMicroseconds(40);
+				}else if(cServoID<12){ /* RightHand Servo Controls (PCA9685 board 2) - Servos 6-11 (6) */
+          #ifdef ENABLE_TRACES
+            Serial.printf("pwm2: ServoID -> %u, OnBoardId -> %u, Position -> %u, PWM signal -> %u, min-maxPWM -> (%u,%u)\n", cServoID, (cServoID-6), cServoPos, pwm, minServoPos[cServoID], maxServoPos[cServoID]);
+          #endif
+
+          pwm2.setPWM((cServoID-6),0,pwm); 
+          delayMicroseconds(40);
+				}else{ /* Body/Head Servo Controls (PCA9685 board 1) - Servos 12-26 (15) */
+          #ifdef ENABLE_TRACES
+            Serial.printf("pwm1: ServoID -> %u, OnBoardId -> %u, Position -> %u, PWM signal -> %u, min-maxPWM -> (%u,%u)\n", cServoID, (cServoID-12), cServoPos, pwm, minServoPos[cServoID], maxServoPos[cServoID]);
+          #endif
+
+          pwm1.setPWM((cServoID-12),0,pwm);        
+          delayMicroseconds(40);   
+				}
       }
       client.print("ACK");
       break;
     case 'u': /* Update target positions (For delayed mode) */
-      // Serial.println("Update positions");
+      #ifdef ENABLE_TRACES
+        Serial.println("At Update servo positions");
+      #endif
+
       if(query.length()<(FORMAT_LEN+SERVOPAIR_LEN*query[SERVOCOUNT_OFFSET])){client.print("NACK1");break;} /* Would be Out of bounds */
       if(query[SERVOCOUNT_OFFSET]>SERVOCOUNT){client.print("NACK2");break;} /* Servocounnt missmatch */
       for(i=0;i<query[SERVOCOUNT_OFFSET];i++){
         cServoID=getServoID();
         flag|=(0b1<<cServoID);
         targetServoPos[cServoID]=getServoPos();
-        Serial.println("Servoid:"+String(cServoID)+"Pos:"+String(targetServoPos[cServoID]));
+
+        #ifdef ENABLE_TRACES
+          Serial.printf("ServoID -> %u, Position -> %u\n", cServoID, targetServoPos[cServoID]);
+        #endif       
       }
       client.print("ACK");
       break;
     case 'e': /* Execute all movements (Delayed mode) */
-      Serial.println("Execute movements");
+      #ifdef ENABLE_TRACES
+        Serial.println("Executing loaded movements");
+      #endif
+      
       if(flag==0){client.print("NACK3");break;} /* No movements loaded - Should never happen but still */
       for(i=0;i<SERVOCOUNT;i++){
         if((flag&(0b1<<i))==((0b1<<i))){
 
           if(i<6){/* LeftHand Servo Controls (pwm board 0) - Servos 0-5 (6) */
-            // pwm2.setPWM(i,0, getServoPos() * ((maxServoPos[i]-minServoPos[i])/180) );   
+          // pwm0.setPWM(cServoID,0,minServoPos[cServoID] + (getServoPos() * ((maxServoPos[cServoID]-minServoPos[cServoID])/180)) ); 
           }else if(i<12){ /* RightHand Servo Controls (pwm board 2) - Servos 6-11 (6) */
-            // pwm0.setPWM((i-6),0, getServoPos() * ((maxServoPos[i]-minServoPos[i])/180) );   
+          // pwm2.setPWM((cServoID-6),0,minServoPos[cServoID] + (getServoPos() * ((maxServoPos[cServoID]-minServoPos[cServoID])/180)) );   
           }else{ /* Body/Head Servo Controls (pwm board 1) - Servos 12-26 (15) */
-            // pwm1.setPWM((i-12),0, getServoPos() * ((maxServoPos[i]-minServoPos[i])/180) );   
+          // pwm1.setPWM((cServoID-12),0,minServoPos[cServoID] + (getServoPos() * ((maxServoPos[cServoID]-minServoPos[cServoID])/180)) );   
           }
             
-          delayMicroseconds(10);
+          delayMicroseconds(40);
         }
       }
       flag=0;
@@ -102,9 +131,11 @@ void setup() {
   WiFi.begin(WIFI_SSID, WIFI_PSSW);
 
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.print("Stuck at wifi connection: ");
-    Serial.println(WiFi.waitForConnectResult());
-    // WiFi.reconnect();
+    #ifdef ENABLE_TRACES
+      Serial.print("Stuck at wifi connection: ");
+      Serial.println(WiFi.waitForConnectResult());
+      // WiFi.reconnect();
+    #endif
     delay(1000);
   }
 
@@ -119,31 +150,68 @@ void setup() {
   pwm2.begin();
   pwm2.setOscillatorFrequency(OSC_FREQ);
   pwm2.setPWMFreq(SERVO_FREQ); 
+
+  strcpy(loginQuery, "!s-NodeMCU_here-Maroon-\x1B-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-e!");
+    for (size_t cnt = 0; cnt < 27; cnt++){
+      loginQuery[25+2*cnt]=(char)(restingServoPos[cnt] +1);
+    }
+  
 }
 
 void loop() {
-  /* Check server connection - reconnect if necessary */
+  /* Check server connection - reconnect if necessary, and set servos to "resting position" */
   if(!client.connected()){
-    if (!client.connect(SERVER_IP, SERVER_PORT)){
-      Serial.println("Connection to server failed");
+    if (!client.connect(SERVER_IP, SERVER_PORT)){      
+      #ifdef ENABLE_TRACES
+        Serial.println("Connection to server failed");
+      #endif
       delay(1000);
       return;
     }else{
       /* Say hello to server and stop reseting the connection */
-      Serial.println("Connecting to server...");
-      client.print("!s-NodeMCU_here-Maroon-\x1B-\1-\1-\1-\1-\1-\1-\1-\1-\1-\1-\1-\1-\1-\1-\1-\1-\1-\1-\1-\1-\1-\1-\1-\1-\1-\1-\1-e!");
-      Serial.println("Setting up servos...");
+      #ifdef ENABLE_TRACES
+        Serial.println("Connecting to server...");
+      #endif
+
+      delay(1000); /* Dont DoS the server */
+      client.print(loginQuery);
+
+      #ifdef ENABLE_TRACES
+        Serial.println("Setting up servos...");
+      #endif
+
       for(i=0;i<SERVOCOUNT;i++){
-        if(i<6){/* LeftHand Servo Controls (PCA9685 board 0) - Servos 0-5 (6) */
-          // pwm0.setPWM(i,0,restingServoPos[i]);   
-        }else if(i<12){ /* RightHand Servo Controls (PCA9685 board 2) - Servos 6-11 (6) */
-          // pwm2.setPWM((i-6),0,restingServoPos[i]);   
-        }else{ /* Body/Head Servo Controls (PCA9685 board 1) - Servos 12-26 (15) */
-          // pwm1.setPWM((i-12),0,restingServoPos[i]);   
-        }
+
+        cServoID=i;
+        cServoPos=restingServoPos[i];
+        pwm = minServoPos[cServoID] + ((float)cServoPos / 179.0f) * (maxServoPos[cServoID] - minServoPos[cServoID]);
+
+        if(cServoID<6){/* LeftHand Servo Controls (PCA9685 board 0) - Servos 0-5 (6) */
+          #ifdef ENABLE_TRACES
+            Serial.printf("pwm0: ServoID -> %u, Position -> %u, PWM signal -> %u, min-maxPWM -> (%u,%u)\n", cServoID, cServoPos, pwm, minServoPos[cServoID], maxServoPos[cServoID]);
+          #endif
+
+          pwm0.setPWM(cServoID,0,pwm); 
+				}else if(cServoID<12){ /* RightHand Servo Controls (PCA9685 board 2) - Servos 6-11 (6) */
+          #ifdef ENABLE_TRACES
+            Serial.printf("pwm2: ServoID -> %u, OnBoardId -> %u, Position -> %u, PWM signal -> %u, min-maxPWM -> (%u,%u)\n", cServoID, (cServoID-6), cServoPos, pwm, minServoPos[cServoID], maxServoPos[cServoID]);
+          #endif
+
+          pwm2.setPWM((cServoID-6),0,pwm); 
+				}else{ /* Body/Head Servo Controls (PCA9685 board 1) - Servos 12-26 (15) */
+          #ifdef ENABLE_TRACES
+            Serial.printf("pwm1: ServoID -> %u, OnBoardId -> %u, Position -> %u, PWM signal -> %u, min-maxPWM -> (%u,%u)\n", cServoID, (cServoID-12), cServoPos, pwm, minServoPos[cServoID], maxServoPos[cServoID]);
+          #endif
+
+          pwm1.setPWM((cServoID-12),0,pwm);           
+				}
+
         delayMicroseconds(10);
       }
-      Serial.println("READY");
+      #ifdef ENABLE_TRACES
+        Serial.println("READY");
+        Serial.println("Listening for server orders ...");
+      #endif
     }
   }
   /* Control loop */
@@ -152,7 +220,7 @@ void loop() {
     do{
       query+=(char)client.read();
     }while(client.available());
-    // Serial.println(query);
+
     handleQuery();
 
   }
--- codeMCU/robotControl_Server/servoCalibration.h
+++ codeMCU/robotControl_Server/servoCalibration.h
@@ -8,12 +8,12 @@
 // #define SERVOMAX  455 // This is the 'maximum' pulse length count (out of 4096) AKA 180º
 
 //yellow color on servo // White marking on cable
-#define SERVOMIN_Rotation_Biceps_y  71 
-#define SERVOMAX_Rotation_Biceps_y  410 
-#define SERVOMIN_Codo_y  220 
-#define SERVOMAX_Codo_y  455 
-#define SERVOMIN_Rotation_Antebrazo_y  71 
-#define SERVOMAX_Rotation_Antebrazo_y  400 
+#define SERVOMIN_Rotation_Biceps_L  71 
+#define SERVOMAX_Rotation_Biceps_L  410 
+#define SERVOMIN_Codo_L  220 
+#define SERVOMAX_Codo_L  455 
+#define SERVOMIN_Rotation_Antebrazo_L  71 
+#define SERVOMAX_Rotation_Antebrazo_L  400 
 //yellow color on servo // White marking on cable
 
 //The other arm
@@ -30,9 +30,9 @@
 #define SERVOMAX_Rotation_Head 349 
 #define SERVOMIN_Rotation_Head 167
 //Tilt With marker
-#define SERVOMIDDLE_tiltHead_y 250
-#define SERVOMIN_tiltHead_y 75
-#define SERVOMAX_tiltHead_y 445
+#define SERVOMIDDLE_tiltHead_L 250
+#define SERVOMIN_tiltHead_L 75
+#define SERVOMAX_tiltHead_L 445
 //Tilt NO marker
 #define SERVOMIDDLE_tiltHead 260
 #define SERVOMIN_tiltHead 75
@@ -41,77 +41,130 @@
 
 //Shoulder movements
 //Not marked side
-#define SERVOMAX_shoulderarm 455 //Resting position, parallel with body
-#define SERVOMIN_shoulderarm 140 //perpendicular with body
-#define SERVOMAX_shoulderbody 455 //Upwards motion of the arm
-#define SERVOMIN_shoulderbody 260 //Resting position, parallel with body - ish
+#define SERVOMAX_shoulderarm 455 
+#define SERVOMIN_shoulderarm 140 
+#define SERVOMAX_shoulderbody 455 
+#define SERVOMIN_shoulderbody 260 
 //marked side
-#define SERVOMAX_shoulderarm_y 200 //Resting position, parallel with body
-#define SERVOMIN_shoulderarm_y 440 //perpendicular with body
-#define SERVOMAX_shoulderbody_y 455 //Resting position, parallel with body - ish
-#define SERVOMIN_shoulderbody_y 320 //Upwards motion of the arm
+#define SERVOMAX_shoulderarm_L 200 
+#define SERVOMIN_shoulderarm_L 440 
+#define SERVOMAX_shoulderbody_L 455 
+#define SERVOMIN_shoulderbody_L 320 
 //Shoulder movements
 
 //Right hand 
-// thumb 0, index 1, middle 2, ring 3, pinky 4, palm 5
-#define SERVOMIN_pinky_R 130 //green
-#define SERVOMAX_pinky_R 370
+// thumb 0, index 1, middle 2, ring 3, little 4, palm 5
+#define SERVOMIN_little_R 130 //green
+#define SERVOMAX_little_R 370
 #define SERVOMIN_ring_R 150 //blue
 #define SERVOMAX_ring_R 400
 #define SERVOMIN_middle_R 150 //yellow
 #define SERVOMAX_middle_R 420 //dedo estirado
 #define SERVOMIN_index_R 150 //red
 #define SERVOMAX_index_R 400
-#define SERVOMIN_thumb_R 0 //HUH?
-#define SERVOMAX_thumb_R 0 //HUH?
+#define SERVOMIN_thumb_R 100 // no color
+#define SERVOMAX_thumb_R 400 
 #define SERVOMIN_palm_R 230 //gray
 #define SERVOMAX_palm_R 450
 //Left Hand
-#define SERVOMIN_pinky_L 170
-#define SERVOMAX_pinky_L 370
+#define SERVOMIN_little_L 170
+#define SERVOMAX_little_L 370
 #define SERVOMIN_ring_L 170 //blue
 #define SERVOMAX_ring_L 370
 #define SERVOMIN_middle_L 170 //Yellow
 #define SERVOMAX_middle_L 370
-#define SERVOMIN_index_L 170//red
+#define SERVOMIN_index_L 170 //red
 #define SERVOMAX_index_L 370
-#define SERVOMIN_thumb_L 200
-#define SERVOMAX_thumb_L 450
+#define SERVOMIN_thumb_L 210 // no color
+#define SERVOMAX_thumb_L 440
 #define SERVOMIN_palm_L 280 //mano cerrada
 #define SERVOMAX_palm_L 420
 
 //MAX es dedo estirado, min es puño cerrado
 
 //Torso
-#define SERVOMIN_torso_y 90 //Max rotation up
-#define SERVOMAX_torso_y 450 //Max rotation down
-#define SERVOMIDDLE_torso_y 290 //Hold position-Middle position
+#define SERVOMIN_torso_L 90 //Max rotation up
+#define SERVOMAX_torso_L 450 //Max rotation down
+#define SERVOMIDDLE_torso_L 290 //Hold position-Middle position (100)
 
 #define SERVOMIN_torso 90 //Max rotation down
 #define SERVOMAX_torso 450 //Max rotation up
-#define SERVOMIDDLE_torso 280 //Hold position-Middle position
+#define SERVOMIDDLE_torso 280 //Hold position-Middle position (95)
 
 // Total Number of servos
 #define SERVOCOUNT 27
 
 // Servo Information for server control/connection
 /** Minimum pulse lenght for the PWM signal of each servo -> minServoPos[servoID] >= 71 */
-const uint16_t minServoPos[]={SERVOMIN_thumb_L,SERVOMIN_index_L,SERVOMIN_middle_L,SERVOMIN_ring_L,SERVOMIN_pinky_L,SERVOMIN_palm_L,SERVOMIN_thumb_R,SERVOMIN_index_R,SERVOMIN_middle_R,SERVOMIN_ring_R,SERVOMIN_pinky_R,SERVOMIN_palm_R,SERVOMIN_Rotation_Antebrazo,SERVOMIN_Rotation_Antebrazo_y,SERVOMIN_Codo,SERVOMIN_Codo_y,SERVOMIN_Rotation_Biceps,SERVOMIN_Rotation_Biceps_y,SERVOMIN_shoulderarm,SERVOMIN_shoulderarm_y,SERVOMIN_shoulderbody,SERVOMIN_shoulderbody_y,SERVOMIN_torso,SERVOMIN_torso_y,SERVOMIN_tiltHead,SERVOMIN_tiltHead_y,SERVOMIN_Rotation_Head};
+const uint16_t minServoPos[]={SERVOMIN_thumb_L,SERVOMIN_index_L,SERVOMIN_middle_L,SERVOMIN_ring_L,SERVOMIN_little_L,SERVOMIN_palm_L,
+                              SERVOMIN_thumb_R,SERVOMIN_index_R,SERVOMIN_middle_R,SERVOMIN_ring_R,SERVOMIN_little_R,SERVOMIN_palm_R,
+                              SERVOMIN_Rotation_Antebrazo_L,SERVOMIN_Rotation_Antebrazo,
+                              SERVOMIN_Codo_L,SERVOMIN_Codo,
+                              SERVOMIN_Rotation_Biceps_L,SERVOMIN_Rotation_Biceps,
+                              SERVOMIN_shoulderarm_L,SERVOMIN_shoulderarm,
+                              SERVOMIN_shoulderbody_L,SERVOMIN_shoulderbody,
+                              SERVOMIN_torso_L,SERVOMIN_torso,
+                              SERVOMIN_tiltHead_L,SERVOMIN_tiltHead,SERVOMIN_Rotation_Head};
+
 /** Maximum pulse lenght for the PWM signal of each servo -> maxServoPos[servoID] <= 455 */
-const uint16_t maxServoPos[]={SERVOMAX_thumb_L,SERVOMAX_index_L,SERVOMAX_middle_L,SERVOMAX_ring_L,SERVOMAX_pinky_L,SERVOMAX_palm_L,SERVOMAX_thumb_R,SERVOMAX_index_R,SERVOMAX_middle_R,SERVOMAX_ring_R,SERVOMAX_pinky_R,SERVOMAX_palm_R,SERVOMAX_Rotation_Antebrazo,SERVOMAX_Rotation_Antebrazo_y,SERVOMAX_Codo,SERVOMAX_Codo_y,SERVOMAX_Rotation_Biceps,SERVOMAX_Rotation_Biceps_y,SERVOMAX_shoulderarm,SERVOMAX_shoulderarm_y,SERVOMAX_shoulderbody,SERVOMAX_shoulderbody_y,SERVOMAX_torso,SERVOMAX_torso_y,SERVOMAX_tiltHead,SERVOMAX_tiltHead_y,SERVOMAX_Rotation_Head};
-/** Target angle the servo should move to -> targetServoPos[servoID] = [0,180] */
+const uint16_t maxServoPos[]={SERVOMAX_thumb_L,SERVOMAX_index_L,SERVOMAX_middle_L,SERVOMAX_ring_L,SERVOMAX_little_L,SERVOMAX_palm_L,
+                              SERVOMAX_thumb_R,SERVOMAX_index_R,SERVOMAX_middle_R,SERVOMAX_ring_R,SERVOMAX_little_R,SERVOMAX_palm_R,
+                              SERVOMAX_Rotation_Antebrazo_L,SERVOMAX_Rotation_Antebrazo,
+                              SERVOMAX_Codo_L,SERVOMAX_Codo,
+                              SERVOMAX_Rotation_Biceps_L,SERVOMAX_Rotation_Biceps,
+                              SERVOMAX_shoulderarm_L,SERVOMAX_shoulderarm,
+                              SERVOMAX_shoulderbody_L,SERVOMAX_shoulderbody,
+                              SERVOMAX_torso_L,SERVOMAX_torso,
+                              SERVOMAX_tiltHead_L,SERVOMAX_tiltHead,SERVOMAX_Rotation_Head};
+/** Target angle the servo should move to -> targetServoPos[servoID] = [0,179] */
 uint8_t targetServoPos[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-/** Resting angle position the servo should be initialized to -> restingServoPos[servoID] = [0,180] */
-const uint8_t restingServoPos[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+/** Resting angle position the servo should be initialized to -> restingServoPos[servoID] = [0,179] */
+const uint8_t restingServoPos[]={90,90,90,90,90,0, // left hand
+                                  90,90,90,90,90,179, //right hand
+                                  90,90, // forearm rotation
+                                  170,60, // arm flex
+                                  110,70, // arm rotation
+                                  150,150, // shoulder flex
+                                  0,179, // circonduction
+                                  100,95, // torso tilt
+                                  56,126, // head tilt
+                                  100}; // head rot
 
 
-//Board1 - Left Hand(6) // Board3 - Right Hand(6)
+//Board1 - Left Hand (6 servos) // Board3 - Right Hand (6 servos)
 //Board1/3 ids
-// thumb 0, index 1, middle 2, ring 3, pinky 4, palm 5
-// Board2 - Arms/Torso/Head(3x2/6/3)
+// thumb 0, index 1, middle 2, ring 3, little 4, palm 5
 
-//Board2 (Body/Head) ids
+
+/* Board2 - Arms,Torso,Head (15 servos) */
+//Board2 ids
 // forearmRotationL - 0 / armFlexL - 2 / bicepsRotationL - 4 / shoulderArmRotationL - 6 / shoulderTorsoRotationL - 8
 // forearmRotationR - 1 / armFlexR - 3 / bicepsRotationR - 5 / shoulderArmRotationR - 7 / shoulderTorsoRotationR - 9
 // torsoTiltL - 10 / torsoTiltR - 11
-// headTiltL - 12 / headTiltR - 13 / headRotation - 14
+// headTiltL - 13 / headTiltR - 14 / headRotation - 15
+
+/*
+HANDS - 0º max flex fingers -> 179º max extension {@90º resting position}
+PALM - 0º thumb towards "the outside" -> 179º thumb towards the inside (of the palm) {@180º resting position RIGHT HAND}
+PALM - 179º thumb towards "the outside" -> 0º thumb towards the inside (of the palm) {@0º resting position LEFT HAND}
+
+ARM (RIGHT)
+  FLEXION -> 180 fully extended, 60º resting position
+  SHOULDER tilt -> 0º (T-pose), 150º resting positon
+  ARM rotation -> 180º towards the body, 0º outward rotation; 70º resting position
+  forearm rotation -> 180 palm down, 0 palm up; 90 resting position
+
+ARM (LEFT)
+  FLEXION -> 180º fully contracted, 0º fully extended; 170º resting position
+  SHOULDER tilt -> 0º (T-pose), 150º resting positon
+  ARM rotation -> 180 outwards, 0 towards body; 100 resting position
+  forearm rotation -> 0 palm down, 180 palm up - 90 resting position
+
+TORSO
+  Circunduction RIGHT -> 180 (upwards), 0 (downwards), resting position 180
+  Circunduction LEFT -> 0 (upwards), 180 (downwards), resting position 0
+
+HEAD
+  rotation -> 0º left, 180 right - 100 resting position
+  tilt LEFT -> 0º
+*/
